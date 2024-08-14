f49040dd6e97b01206e81a47a838f306714267c2
Keola Teson <84607397+keola-teson@users.noreply.github.com>
2024-08-14 10:30:13 -1000

    Update Hello_World.java

--- Hello_World.java
+++ Hello_World.java
@@ -1,1 +1,16 @@
+/*
+ * This Code is for the very first assignment. It does two simple prints and then I submit it.
+ */
+public class Hello_World {
 
+	public static void main(String[] args) {
+		
+		//Task 1: Print Hello World
+		System.out.print("Hello World!");
+		//Task 2: Print Hello World again then again on the next line
+		System.out.println("Hello World!");
+		System.out.print("Hello World!");
+		
+	}
+
+}
