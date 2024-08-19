4a6c014039f16aa565a60d18c4e36ee4f992273b
Keola Teson <84607397+keola-teson@users.noreply.github.com>
2024-08-19 12:51:25 -1000

    Create Debug_and_Errors_Practice.java

---
+++ Debug_and_Errors_Practice.java
@@ -0,0 +1,54 @@
+/*
+  For each task below, explain why the code is incorrect, what type of error that is, and how to fix it.
+  Comment block is used as answer.
+*/
+
+//Task 1: Print out "Hello World"
+System.out.print("Hello World);
+/*
+  Syntax Error
+  Missing Quotation Mark at the end
+  Fix: put end quotation after "World"
+*/
+
+//Task 2: Print out "Hello World"
+System.out.print(Hello World);
+/*
+  Syntax Error
+  Missing quotation mark around Hello World
+  Fix: put quotation marks around "Hello World"
+*/
+
+//Task 3: Print out "Hello World"
+System.Out.Print("Hello World");
+/*
+  Syntax Error
+  Miss typed case sensitive command
+  Fix: lowercase "out.print"
+*/
+
+
+//Task 4: Print out "Hello World"
+System.out.print("Hello World")
+/*
+  Syntax Error
+  Forgot semi colon at the end of the line
+  Fix: put a semi colon after the closed parenthesese ");"
+*/
+
+//Task 5: Print out "Hello World"
+System.out.print("Hello Wrold")
+/*
+  Logical and Syntax Error
+  Forgot semi colon at the end of the line and misspelt "World"
+  Fix: put a semi colon after the closed parenthesese ");" and return to second grade
+*/
+
+//Task 6: Print out "Hello World twice, one on top of the other"
+System.out.print("Hello World");
+System.out.print("Hello World");
+/*
+  Logical Error
+  Forgot "ln" after "print" to bring the second "Hello World" down to the next line
+  Fix: insert "ln" after "print" (System.out.println("Hello World"))
+*/
