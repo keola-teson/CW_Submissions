b33d112e6f16a267a05e0965308c0bbcf238fb07
Keola Teson <84607397+keola-teson@users.noreply.github.com>
2024-08-26 12:40:58 -1000

    Create operator_problems.java

---
+++ operator_problems.java
@@ -0,0 +1,14 @@
+//PART ONE: MODULUS
+//What would each of the following 1-6 lines including the modulo operator cause to be printed?
+     System.out.println(14 % 13);
+//prints 1
+     System.out.println(8 % 11);
+//prints 8
+     System.out.println(6 % 2);
+//prints 0
+     System.out.println(7 % 2);
+//prints 1
+     System.out.println(274 % 10);
+//prints 4
+     System.out.println(881 % 2);
+//prints 1
