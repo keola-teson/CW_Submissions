5606d08d6fdceb79e6d64a9c44bbc121429db981
Keola Teson <84607397+keola-teson@users.noreply.github.com>
2024-09-11 18:45:50 -1000

    Update Methods_Intro_Practice.java

--- Methods_Intro_Practice.java
+++ Methods_Intro_Practice.java
@@ -15,62 +15,59 @@ printCircleStats(2);
 System.out.println(" ");
 
 //task 3
-
+printMathStuffs(16, 8);
 
 	}
 
-/*
- * Create a void method called stateName() that prints out your name. 
- * Repeat this task for stateEyeColor(), stateHairStyle(), stateBestFriend(), and stateFavoriteFood(). 
- * Then, call stateName(), stateHairStyle, and stateFavoriteFood().
- */
-
-
 /**
- * This method states my name
+ * Task 1: This method states my name
  */
+	
 public static void stateName()
 {
 	System.out.println("My name is Keola, and I'm the fweakin best.");
 }
+
 /**
  * This method states my eye color
  */
+
 public static void stateEyeColor()
 {
 	System.out.println("I have brown eyes. 0.0");
 }
+
 /**
  * This method states my hair style
  */
+
 public static void stateHairStyle()
 {
 	System.out.println("I have a mullet. Mullet gang.");
 }
+
 /**
  * This method states my best fwiend
  */
+
 public static void stateBestFriend()
 {
 	System.out.println("My best friend is my girlfriend Lyzelle (screw ella).");
 }
+
 /**
  * This method states my favorite food
  */
+
 public static void stateFavoriteFood()
 {
 	System.out.println("My favorite food is Raising Cane's chicken with the Cane's sauce.");
 }
 
-/*
- * Create a method printCircleStats() that is passed a double (meant to represent a circle's radius) 
- * and calculates then prints out the radius, diameter, circumference, and area of the circle. 
- * Call the method to make sure it works.
+/**
+ * Task 2: This method prints circle stats
  */
 
-/**
- * This method prints circle stats
- */
 public static void printCircleStats(double radius)
 {
 	//declare the variables
@@ -89,28 +86,35 @@ public static void printCircleStats(double radius)
 	System.out.print("The diameter of a circle with the radius " + radius + " is "  + diameter + ". ");
 	System.out.print("The area is " + area);
 	System.out.println(", and the volume is " + circ + ".");
-	System.out.print("I'm the best :sunglasses:");
+	System.out.println("I'm the best :sunglasses:");
 }
 
-/*
- * Create a method printMathStuffs() that is passed two ints and prints their sum, 
- * difference, product, quotient, and the remainder (using modulus) of the first number modulus 
- * the second number. Call it to make sure it works.
- */
 
 /**
- * This method does a bunch of math
+ * Task 3: This method does a bunch of math
  */
+
 public static void printMathStuffs(int num1, int num2)
 {
-	//declares 
+	//adds the two then prints
+	num1 += num2;
+	System.out.println(num1);
 	
-	//sets the value of num1 and num2
-	num1 = 1;
-	num2 = 1;
+	//subtracts the two then prints
+	num1 -= num2;
+	System.out.println(num1);
 	
-	//does the math stuff
+	//multiplies the two then prints
+	num1 *= num2;
+	System.out.println(num1);
 	
+	//divides the two then prints
+	num1 /= num2;
+	System.out.println(num1);
+	
+	//finds the remainder of the two
+	num1 %= num2;
+	System.out.println(num1);
 }
 
 }
